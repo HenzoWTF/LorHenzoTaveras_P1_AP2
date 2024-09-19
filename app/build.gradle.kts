b54f409df0aa505e2743813d3f72ebc1c3b2352b
HenzoWTF <Urenahenzo@gmail.com>
2024-09-18 21:18:47 -0400

    Agrega AlgoRepository

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -80,7 +80,7 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-runtime-compose:2.8.4")
 
     // Navigation
-    implementation("androidx.navigation:navigation-compose:2.8.0-rc01")
+    implementation(libs.androidx.navigation.compose)
     implementation(libs.kotlinx.serialization.json)
 
     // Room
