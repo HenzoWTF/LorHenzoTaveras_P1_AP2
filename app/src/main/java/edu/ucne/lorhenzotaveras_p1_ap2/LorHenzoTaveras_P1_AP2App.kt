7a684a13803139e7d2426cf4baa27bdbaf76bc60
HenzoWTF <Urenahenzo@gmail.com>
2024-09-18 20:06:19 -0400

    Agrega Hilt

---
+++ app/src/main/java/edu/ucne/lorhenzotaveras_p1_ap2/LorHenzoTaveras_P1_AP2App.kt
@@ -0,0 +1,9 @@
+package edu.ucne.lorhenzotaveras_p1_ap2
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class LorHenzoTaveras_P1_AP2App :Application(){
+
+}
